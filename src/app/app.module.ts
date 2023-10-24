c8fc1a75c34abd7779edad1cdac44e9e945c611f
Andres Avilan <andresfavilan@hotmail.com>
2023-10-24 10:47:19 -0500

    fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Countercomponent } from './counter/counter.component';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Countercomponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
